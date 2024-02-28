20165391477f8485dbd6712df9cd2c172e2ad0be
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2024-02-28 16:23:08 +0600

    gradl

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
 
 import java.util.List;
-
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
+        for(String s : args) {
+            if(!com.epam.utils.StringUtils.isPositiveNumber(s)) return false;
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,10 @@
 package com.epam.utils;
 
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        boolean res = org.apache.commons.lang3.StringUtils.isNumeric(str);
+        return res;
     }
 }
