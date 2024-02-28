a4d78b1e124778c6cdb8fbbfadebd89cc399a8d2
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2024-02-28 16:35:53 +0600

    gradFix

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,9 +4,9 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        boolean res = org.apache.commons.lang3.math.NumberUtils.isCreatable(str);
+        boolean res = org.apache.commons.lang3.math.NumberUtils.isNumber(str);
         if(res) {
-            if(str.charAt(0) == '+' || str.charAt(0) == '-') res = false;
+            if(str.charAt(0) == '-') res = false;
         }
         return res;
     }
