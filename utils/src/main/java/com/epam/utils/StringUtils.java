8ba5d79decd7b7a02d8a341139fd1f4279ee25e1
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2024-02-28 16:32:26 +0600

    gradleFix

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,7 +4,10 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        boolean res = org.apache.commons.lang3.StringUtils.isNumeric(str);
+        boolean res = org.apache.commons.lang3.math.NumberUtils.isCreatable(str);
+        if(res) {
+            if(str.charAt(0) == '+' || str.charAt(0) == '-') res = false;
+        }
         return res;
     }
 }
